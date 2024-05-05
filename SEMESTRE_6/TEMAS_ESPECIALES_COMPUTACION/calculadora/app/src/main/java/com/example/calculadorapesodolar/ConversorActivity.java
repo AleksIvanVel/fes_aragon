8c85f8d38fb7bf548664d8fd31725112de34749d
velalmanez <velalmanez@gmail.com>
2024-05-05 02:41:03 -0600

    UPDATE app calculadora

--- SEMESTRE_6/TEMAS_ESPECIALES_COMPUTACION/calculadora/app/src/main/java/com/example/calculadorapesodolar/ConversorActivity.java
+++ SEMESTRE_6/TEMAS_ESPECIALES_COMPUTACION/calculadora/app/src/main/java/com/example/calculadorapesodolar/ConversorActivity.java
@@ -7,9 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
 
 public class ConversorActivity extends AppCompatActivity {
 
+    private EditText campoDolar;
+    private EditText campoPesos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,32 @@ public class ConversorActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        campoDolar = (EditText) findViewById(R.id.dolar);
+        campoPesos = (EditText) findViewById(R.id.pesos);
+
+        Button dolarAPesos = (Button) findViewById(R.id.dolar2pesos);
+        Button pesosADolar = (Button) findViewById(R.id.pesos2dolar);
+
+        dolarAPesos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try{
+                    campoDolar.setText(""+Double.parseDouble(campoDolar.getText().toString())*19.92);
+                }catch (Exception e){
+                    campoPesos.setText(getResources().getString(R.string.valorInvalido));
+                }
+            }
+        });
+        pesosADolar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try{
+                    campoPesos.setText(""+Double.parseDouble(campoPesos.getText().toString())*19.92);
+                }catch (Exception e){
+                    campoPesos.setText(getResources().getString(R.string.valorInvalido));
+                }
+
+            }
+        });
+
     }
 }
