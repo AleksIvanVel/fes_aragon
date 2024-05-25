dfd984d5ad9d8946011b11a7f3f5b31ee830d75b
AleksIvanVel <aleksvelazquez32@aragon.unam.mx>
2024-05-25 11:43:27 -0600

    ADD botones salvar y recuperar Datos

--- SEMESTRE_6/TEMAS_ESPECIALES_COMPUTACION/practica_botones/app/src/main/java/com/example/prog_if_consumo/MainActivity.java
+++ SEMESTRE_6/TEMAS_ESPECIALES_COMPUTACION/practica_botones/app/src/main/java/com/example/prog_if_consumo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.prog_if_consumo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private EditText montoCompra;
     private TextView tv1, tv2, tv3, tv4, tv5;
+
+    private Button btnSave, btnLoad;
+
+    private static final String FILE_NAME = "data.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
         tv3=(android.widget.TextView)findViewById(R.id.costoNeto);
         tv4=(android.widget.TextView)findViewById(R.id.impuesto);
         tv5=(android.widget.TextView)findViewById(R.id.importeNeto);
+        btnSave = findViewById(R.id.btnSave);
+        btnLoad = findViewById(R.id.btnLoad);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadData();
+            }
+        });
     }
     public void calcular_descuento(View view) {
         double dscto,vventa,impuestos,vfactura;
@@ -48,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // calcular valor venta
-        vventa = cantconsumo * dscto;
+        vventa = cantconsumo - dscto;
 
         // calcular impuesto
         impuestos = vventa * 0.18;
@@ -69,5 +96,45 @@ public class MainActivity extends AppCompatActivity {
         tv3.setText(resu2);
         tv4.setText(resu3);
         tv5.setText(resu4);
+    }
+
+    private void saveData() {
+        String data = tv1.getText().toString() + "\n" +
+                tv2.getText().toString() + "\n" +
+                tv3.getText().toString() + "\n" +
+                tv4.getText().toString() + "\n" +
+                tv5.getText().toString();
+
+        try (FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE)) {
+            fos.write(data.getBytes());
+            Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error al guardar los datos", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void loadData() {
+        try (FileInputStream fis = openFileInput(FILE_NAME)) {
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            String data = new String(buffer);
+
+            String[] lines = data.split("\n");
+            if (lines.length >= 5) {
+                tv1.setText(lines[0]);
+                tv2.setText(lines[1]);
+                tv3.setText(lines[2]);
+                tv4.setText(lines[3]);
+                tv5.setText(lines[4]);
+                Toast.makeText(this, "Data loaded: " + data, Toast.LENGTH_LONG).show();  // Show loaded data
+            } else {
+                Toast.makeText(this, "Data format error", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            Toast.makeText(this, "Failed to load data", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
